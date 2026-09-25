4139b461628e81bb94931345eac73156cba2e15d
Shahabuzar Akhlaq <abuzaraq3@gmail.com>
2026-09-25 20:30:41 +0530

    feat: major updates on categories functionality, auth, permissions, README.md

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -52,6 +52,7 @@ INSTALLED_APPS = [
     # apps
     'blog',
     'users',
+    'dashboards',
 ]
 
 MIDDLEWARE = [
---
+++ backend/dashboards/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path, include
+from . import views
+
+urlpatterns = [
+    path('dashboard/', views.dashboard, name='dashboard'),
+    path('dashboard/categories', views.categories, name='categories'),
+    path('dashboard/categories/add/', views.add_category, name='add_category'),
+    path('dashboard/categories/edit/<int:pk>', views.edit_category, name='edit_category'),
+    path('dashboard/categories/delete/<int:pk>', views.delete_category, name='delete_category'),
+
+
+]
